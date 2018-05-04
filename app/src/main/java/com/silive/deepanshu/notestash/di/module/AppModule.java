b2cb57edaf96a473cfb3ccec1a811a853d7da130
deepanshut041 <deepanshut041@gmail.com>
2018-05-04 23:29:48 +0530

    Update the AppModule to provide the injector for the AuthActivity

--- app/src/main/java/com/silive/deepanshu/notestash/di/module/AppModule.java
+++ app/src/main/java/com/silive/deepanshu/notestash/di/module/AppModule.java
@@ -1,7 +1,16 @@
 package com.silive.deepanshu.notestash.di.module;
 
+import android.app.Activity;
+
+import com.silive.deepanshu.notestash.di.subcomponent.AuthActivitySubcomponent;
+import com.silive.deepanshu.notestash.ui.auth.AuthActivity;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.android.ActivityKey;
 import dagger.android.AndroidInjectionModule;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
 
 /**
  * Created by deepanshu on 3/5/18.
@@ -14,5 +23,17 @@ import dagger.android.AndroidInjectionModule;
 
 @Module(includes = AndroidInjectionModule.class)
 public abstract class AppModule {
+
+    // TODO (ContributesAndroidInjector) remove this in favor of @ContributesAndroidInjector
+
+    /**
+     * authActivityInjectorFactory provides the injector for the AuthActivity.
+     * The AuthActivitySubcomponent has also been added as a subcomponent in the @Module subcomponents field.
+     */
+    @Binds
+    @IntoMap
+    @ActivityKey(AuthActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity>
+    mainActivityInjectorFactory(AuthActivitySubcomponent.Builder builder);
 
 }
